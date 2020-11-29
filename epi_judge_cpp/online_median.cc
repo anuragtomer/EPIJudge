6366ee99686ff00ed051b610692720c3363376e9
Anurag Tomer <anuragtomer12@gmail.com>
2020-11-29 11:08:37 +0530

    11.05 Online Median book solution

--- epi_judge_cpp/online_median.cc
+++ epi_judge_cpp/online_median.cc
@@ -5,18 +5,16 @@ using std::greater;
 using std::less;
 using std::priority_queue;
 using std::vector;
-
+/* One way
 void balance(priority_queue<int, vector<int>, greater<>>& min_heap,
              priority_queue<int, vector<int>, less<>>& max_heap,
              bool is_min_heap_larger) {
   if (is_min_heap_larger) {
-    auto top_elem = min_heap.top();
+    max_heap.emplace(min_heap.top());
     min_heap.pop();
-    max_heap.emplace(top_elem);
   } else {
-    auto top_elem = max_heap.top();
+    min_heap.emplace(max_heap.top());
     max_heap.pop();
-    min_heap.emplace(top_elem);
   }
 }
 
@@ -52,8 +50,37 @@ vector<double> OnlineMedian(vector<int>::const_iterator sequence_begin,
     sequence_begin = next(sequence_begin);
   }
   return result;
+}*/
+vector<double> OnlineMedian(vector<int>::const_iterator sequence_begin,
+                            const vector<int>::const_iterator& sequence_end) {
+  priority_queue<int, vector<int>, greater<>> min_heap;
+  priority_queue<int, vector<int>, less<>> max_heap;
+  int x;
+  vector<double> result;
+  while (sequence_begin != sequence_end) {
+    x = *sequence_begin;
+    if (min_heap.empty()) {
+      min_heap.emplace(x);
+    } else {
+      if (x >= min_heap.top())
+        min_heap.emplace(x);
+      else
+        max_heap.emplace(x);
+    }
+    if (min_heap.size() > max_heap.size() + 1) {
+      max_heap.emplace(min_heap.top());
+      min_heap.pop();
+    } else if (max_heap.size() > min_heap.size()) {
+      min_heap.emplace(max_heap.top());
+      max_heap.pop();
+    }
+    result.push_back(min_heap.size() == max_heap.size()
+                         ? 0.5 * (min_heap.top() + max_heap.top())
+                         : min_heap.top());
+    sequence_begin = next(sequence_begin);
+  }
+  return result;
 }
-
 vector<double> OnlineMedianWrapper(const vector<int>& sequence) {
   return OnlineMedian(cbegin(sequence), cend(sequence));
 }
