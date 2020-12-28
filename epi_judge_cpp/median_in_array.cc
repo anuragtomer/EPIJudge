cef90a8a8561fe86bb79636dcd3a559cea591525
Anurag Tomer <anuragtomer12@gmail.com>
2020-12-28 08:15:16 +0530

    12.08.01 Find Median in array

--- epi_judge_cpp/median_in_array.cc
+++ epi_judge_cpp/median_in_array.cc
@@ -2,19 +2,58 @@
 #include <vector>
 
 #include "test_framework/generic_test.h"
-using std::swap;
 using std::vector;
 
-// The numbering starts from one, i.e., if A = [3, 1, -1, 2] then
-// FindKthLargest(1, A) returns 3, FindKthLargest(2, A) returns 2,
-// FindKthLargest(3, A) returns 1, and FindKthLargest(4, A) returns -1.
-
-int FindMedian(vector<int>* A_ptr) {
-  // Your code here.
-  return 0;
+template <typename Compare>
+int partition(vector<int>* A_ptr, int left, int right, int pivot_index,
+              Compare comp) {
+  vector<int>& A = *A_ptr;
+  int pivot_value = A[pivot_index];
+  int new_pivot_index = left;
+  std::swap(A[right], A[pivot_index]);
+  right--;
+  for (int i = left; i <= right; ++i) {
+    if (comp(A[i], pivot_value)) {
+      std::swap(A[i], A[new_pivot_index++]);
+    }
+  }
+  std::swap(A[right + 1], A[new_pivot_index]);
+  return new_pivot_index;
 }
 
-int FindMedianWrapper(vector<int>& A) { return FindMedian(&A); }
+int FindKth(int k, vector<int>* A_ptr) {
+  vector<int>& A = *A_ptr;
+  int left = 0, right = A.size() - 1;
+  std::default_random_engine gen((std::random_device())());
+  while (left <= right) {
+    int pivot_index = std::uniform_int_distribution<int>{left, right}(gen);
+    int new_pivot_index =
+        partition(&A, left, right, pivot_index, std::less<int>());
+    if (new_pivot_index == k - 1)
+      return A[new_pivot_index];
+    else if (new_pivot_index > k - 1)
+      right = new_pivot_index - 1;
+    else
+      left = new_pivot_index + 1;
+  }
+  assert(false);
+  return -1;
+}
+
+float FindMedian(vector<int>* A_ptr) {
+  vector<int>& A = *A_ptr;
+  size_t sz = A.size();
+  int k = sz / 2;
+  int right = FindKth(k + 1, &A);
+  if (sz % 2 == 1)
+    return static_cast<float>(right);
+  else {
+    int left = FindKth(k, &A);
+    return 0.5 * static_cast<float>(left + right);
+  }
+}
+
+float FindMedianWrapper(vector<int>& A) { return FindMedian(&A); }
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
